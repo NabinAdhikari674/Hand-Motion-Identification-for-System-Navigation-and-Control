ea2f62124b533bf0e768fe98936ce0129b0306b3
Nabin Adhikari <36256500+NabinAdhikari674@users.noreply.github.com>
2019-11-13 14:38:31 +0530

    Updated for Restructured Project System Design

--- sources/gesture_verify.py
+++ sources/gesture_verify.py
@@ -4,35 +4,14 @@
 
 print("\n\t\t\t##Running ","gesture-verify-test.py##");
 print("Setting Global Variables and Packages...",end=' ')
-import logging
-logging.getLogger('tensorflow').disabled = True
 import numpy as np
+import custom_utilities
+import global_var_tunnel as gv
 flags=[];
 names = ['1','2','3','4','5','Palm','Fist']
 d1=d2=d3=d4=d5=dFist=dPalm=''
 Gesture_1=Gesture_2=Gesture_3=Gesture_4=Gesture_5=Gesture_Fist=Gesture_Palm=[]
 print("## Done.")
-
-def NotFoundSoln(ftypes,title):
-    print("\n\t\t!!! Model not Found !!!")
-    choose=str(input("\n\t\tOpen File Picker to Choose Model to load?\nEnter [y/n]:   "))
-    if choose=='y':
-        from tkinter import Tk
-        from tkinter.filedialog import askopenfilename
-        root=Tk()
-        filename = askopenfilename(filetypes = ftypes,title = title)
-        root.withdraw()
-        print ("This is Chosen FilePath : ",filename)
-        return filename
-    elif choose=='n':
-        print("\n\t\tThere is no Model to Load")
-        print("Prediction without model is Impossible.So a model has to be Loaded or Trained.")
-        print("To train a new model for 'skin_verify' run 'skin_verify.py' and Train model there.")
-        print("Try again to Load a Saved 'Skin_verify_model.pkl' or similar Model")
-        NotFoundSoln(ftypes,title)
-    else:
-        print("Wrong Choice...TRY AGAIN\n")
-        NotFoundSoln(ftypes,title)
 
 def DataReader(dir_path):
     print("\n\t\t\t##Running ","DataReader Function##");
@@ -91,30 +70,10 @@ def DataPrepper():
         print("Read DONE.\n")
     except FileNotFoundError:
         print("\n\n\t\tImage Folders Not Found !!!")
-        def choser():
-            global flag
-            choose=str(input("\n\t\tOpen File Picker to Choose Folder to open?\nEnter [y/n]:   "))
-            if choose=='y':
-                from tkinter import Tk
-                from tkinter.filedialog import askdirectory
-                root=Tk()
-                root.withdraw()
-                foldername = askdirectory(initialdir=os.getcwd,title="Folder Chooser" )
-                #print ("This is Chosen Folder and Path : ",foldername)
-                print("\tReading Data from Choosen Folder...##",end=" ")
-                dir_path=DataReader(foldername)
-                flags.append(2)
-                return dir_path
-                print("Read DONE.\n")
-            elif choose=='n':
-                print("\n\t\tThere are no Files or Folders to process")
-                print("\n\t\tExiting...")
-                exit()
-            else:
-                print("Wrong Choice...TRY AGAIN\n")
-                choser()
-        dir_path=choser()
-        print("Chooser DONE")
+        foldername=custom_utilities.FileFolderPicker([('Folder')],title='FolderPicker',mode='Folder')
+        dir_path=DataReader(foldername)
+    except Exception as exp:
+        print("\t!! Error !! : \n\t\t",exp)
     finally:
         print("Data LOAD Done")
 
@@ -137,7 +96,7 @@ def DataPrepper():
     return dir_path
     print("\n\n DataPrepper END\n")
 
-def PreProcessor(dir_path):
+def PreProcessor(dir_path='../generatedData/gestureData/'):
     print("\t\t\t##Running ","PreProcessor Function##");
     global flags
     #global d1,d2,d3,d4,d5,DFist,DPalm
@@ -145,31 +104,23 @@ def PreProcessor(dir_path):
 
     try:
         print("Importing Packages Required...",end="##")
-        import tensorflow as tf
-        import keras_preprocessing
-        from keras_preprocessing import image
+        #import tensorflow as tf
+        #import keras_preprocessing
+        #from keras_preprocessing import image
         from keras_preprocessing.image import ImageDataGenerator
         print("...Import Sucessful")
     except:
         print("\n\t##Error in IMPORT...Check if all packages are properly Installed##\n\t")
 
-    TRAINING_DIR = dir_path
+    TRAINING_DIR = dir_path+'TrainingData'
     print("Training Directory is :",TRAINING_DIR)
-    training_datagen = ImageDataGenerator(
-      rescale = 1./255,
-      rotation_range=40,
-      width_shift_range=0.2,
-      height_shift_range=0.2,
-      shear_range=0.2,
-      zoom_range=0.2,
-      horizontal_flip=True,
-      fill_mode='nearest')
-    train_generator = training_datagen.flow_from_directory(TRAINING_DIR,target_size=(150,150),color_mode='rgb',class_mode='categorical')
+    training_datagen = ImageDataGenerator(rescale = 1./255, horizontal_flip=True)
+    train_generator = training_datagen.flow_from_directory(TRAINING_DIR,target_size=(150,150),color_mode='grayscale',class_mode='categorical')
 
-    VALIDATION_DIR='C:/Users/nabin/Desktop/Test Files/DATA/ValidationData/'
-    validation_datagen = ImageDataGenerator(rescale = 1./255)
-    validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,target_size=(150,150),color_mode='rgb',class_mode='categorical')
-    
+    VALIDATION_DIR=dir_path+'ValidationData'
+    validation_datagen = ImageDataGenerator(rescale = 1./255,horizontal_flip=True)
+    validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,target_size=(150,150),color_mode='grayscale',class_mode='categorical')
+
     print("\n\t##Data PreProcessing Done. END\n")
     flags.append(3)
     return (train_generator,validation_generator)
@@ -216,15 +167,90 @@ def NeuralNetBuilder():
     flags.append(7)
     return(model)
 
-def ModelTrainer(model,train_generator,validation_generator):
+def DataSplitter(train_generator,validation_generator):
+    print("=========== Data Preparation going On ... ============\n")
+
+    #xt, yt = zip(*(train_generator[i] for i in range(len(train_generator))))
+    #x_train, y_train = np.vstack(xt), np.vstack(yt)
+    for d, l in validation_generator:
+        data.append(d)
+        labels.append(l)
+        i += 1
+        if i == max_iter:
+            break
+    print("Training Data Length : ",len(x_train),x_train.shape)
+
+
+    #xv,yv = zip(*(validation_generator[i] for i in range(len(validation_generator))))
+    #x_val, y_val = np.vstack(xv), np.vstack(yv)
+    print("Validation Data Length : ",len(x_val),x_val.shape)
+    print(x_val)
+
+    print("============ Data Preparation DONE ... =============\n")
+    return x_train,y_train,x_val,y_val
+
+def SVMModel():
+
+    #from sklearn.svm import SVC
+    from sklearn.tree import DecisionTreeClassifier
+    model=DecisionTreeClassifier()
+
+
+    '''import tensorflow as tf
+
+    model = tf.keras.models.Sequential([
+
+        tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+                                        # The second convolution
+        tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+                                        # The third convolution
+        tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+                                        # The fourth convolution
+        tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(7,kernel_regularizer=tf.keras.regularizers.l2(0.01)), #kernel_regularizer=regularizers.l2(0.01)
+        tf.keras.layers.Dense(7, activation='softmax')
+
+                                      ])
+
+
+    model.compile(loss='squared_hinge',optimizer='adadelta',metrics=['accuracy'])
+    '''
+
+    print("\n\t##SVM Model Built and Compiled(Ready for Training). END\n")
+    flags.append(8)
+    return(model)
+
+def ModelTrainer(*args):
     print("\t\t\t##Running ","ModelTrainerTester Function##");
     global flags
+    model=train_generator=validation_generator=generator=x_train=y_train=None
+    for argument in args:
+        try:
+            model=args[0]
+            train_generator=args[1]
+            validation_generator=args[2]
+            generator=args[3]
+            x_train=args[4]
+            y_train=args[5]
+        except:
+            pass
+            #print("Some Arguments were Missing.But we can Move on...")
+
     #global d1,d2,d3,d4,d5,DFist,DPalm
     #global Gesture_1,Gesture_2,Gesture_3,Gesture_4,Gesture_5,Gesture_Fist,Gesture_Palm
 
     try:
         print("Importing Packages Required...",end="##")
         import tensorflow as tf
+        import logging
+        logging.getLogger('tensorflow').disabled = True
         #from matplotlib import pyplot as plt
         print("...Import Sucessful")
     except:
@@ -233,30 +259,36 @@ def ModelTrainer(model,train_generator,validation_generator):
     trained=0
     choose=str(input("\n\t\tStart the training?\nEnter [y/n]:   "))
     if choose=='y':
-        print("Sit Tight !! Training takes a whole lot of Time.\n")
-        history = model.fit_generator(train_generator,validation_data = validation_generator, epochs=100, verbose = 1)# ,validation_data = validation_generator
+        print("!! Sit Tight !! Training takes a Whole lot of Time.\n")
+        if generator==True:
+            history = model.fit_generator(train_generator,validation_data = validation_generator, epochs=1, verbose = 1)# ,validation_data = validation_generator
+        elif generator==False:
+            history = model.fit(x_train,y_train)
         trained = 1
     if choose=='n':
-        choose=str(input("\n\t\tHalt the Training for Sure?\nEnter [y/n]:   "))
+        choose=str(input("\n\t\t!!! Halt the Training for Sure?\nEnter [y/n]:   "))
         if choose=='y':
             pass
         if choose=='n':
             print("Buckle Up !! You are up for a Long Ride !!\n")
-            history = model.fit_generator(train_generator, epochs=100, verbose = 1)  # ,validation_data = validation_generator
+            if generator==True:
+                history = model.fit_generator(train_generator,validation_data = validation_generator, epochs=1, verbose = 1)# ,validation_data = validation_generator
+            elif generator==False:
+                history = model.fit(x_train,y_train)
             trained = 1
 
     if trained == 1:
         choose=str(input("\n\t\tSave Model to the Disk?\nEnter [y/n]:   "))
         if choose =='y':
-            model.save("gesture_neural_weights1.h5")
-            print("Model Saved !! ('gesture_neural_weights1.h5')")
+            model.save("../generatedData/newModel.h5")
+            print("Model Saved !! ('newModel.h5')")
         if choose =='n':
             choose=str(input("\n\t\tYou Sure to not save the Hard-earned Trained Model?\nEnter [y/n]:   "))
             if choose =='y':
                 pass
             if choose =='n':
-                model.save("gesture_neural_weights1.h5")
-                print("Knew It !! Model Saved as ('gesture_neural_weights1.h5')")
+                model.save("../generatedData/newModel.h5.h5")
+                print("Knew It !! Model Saved as ('newModel.h5')")
     flags.append(4)
     return(model)
 
@@ -299,53 +331,48 @@ def ModelSaver(model):
                 print("!!! Exiting without Saving the model !!!")
         choser1234(model)
 
-def ModelLoader():
+def ModelLoader(loc='models/9G_tanh_model_105906112019.h5'):
     print("\t\t\n  #### Running ModelLoader Function ####\n")
     try:
         print("Importing Packages Required...",end="##")
         from tensorflow.keras.models import load_model
-        from tensorflow.keras.models import model_from_json
+        #from tensorflow.keras.models import model_from_json
         print("...Import Sucessful")
-    except:
-        print("\n\t##Error in IMPORT...Check if all packages are properly Installed##\n\t")
+    except Exception as exp:
+        print("\n\t##Error in IMPORT...Check if all packages are properly Installed##\n\tError:\n\t\t",exp)
 
-    print("Importing Trained Model using \"json\" and \"h5\" Files...##",end=" ")
+    print("Importing Trained Model using  \"h5\" File...##",end=" ")
     try:
-        json_file = open('gesture_neural_model.json', 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
-        model = model_from_json(loaded_model_json)
-    except FileNotFoundError:
-        ftypes = [('Json FIle',"*.json")]
-        tt1  = "Json File Picker"
-        filename1=NotFoundSoln(ftypes,tt1)
-        json_file = open(filename1, 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
-        model = model_from_json(loaded_model_json)
-    try:
-        model.load_weights("gesture_neural_weights.h5")
+        loc='models/9G_tanh_model_105906112019.h5'
+        model=load_model(loc)
+        gv.update('modelUsed',loc)
     except FileNotFoundError:
         ftypes = [('Hierarchical Data Format(HDF)',"*.h5")]
-        tt1 = "Hierarchical Data Format (HDFv5) Picker"
-        filename2 = NotFoundSoln(ftypes,tt1)
-        model.load_weights(filename2)
-    print("\n $$$$$$$$$$ Model Loaded from disk. $$$$$$$$$$$")
+        tt1  = "Hierarchical Data Format (HDFv5) Picker"
+        filename1=custom_utilities.FileFolderPicker(ftypes,tt1)
+        model=load_model(filename1)
+        gv.update(filename1)
+    except Exception as ex:
+        print("!! Error !! : \n\t",ex)
+
+    print("======== Model Loaded from Disk. ========")
+
     return model
+def GesturePredictor(model,array,mode='gray'):
+    if mode=='gray':
+        array = resize(array, (150, 150))
+        array = cast(array, float32)
+        array = np.expand_dims(array,axis=2)
+        array = np.expand_dims(array,axis=0)
+        array = np.vstack([array])
+        prediction = model.predict(array)
+        return prediction
 
-def GesturePredictor(model,array):
-    #img=image.array_to_img(array)
-    #img = image.load_img(images, target_size=(150, 150))
-    #x = image.img_to_array(img)
-    #x = img_to_array(array)
-    #resize=cv2.resize(x,(150,150))
-    #resize=img_to_array(x)
-    #img=resize[np.newaxis,...]
-    img = np.expand_dims(array,axis=0)
-    img = np.vstack([img])
-    prediction5=model.predict(img)
-
-    return prediction5
+    elif mode=='rgb':
+        array = np.expand_dims(array,axis=0)
+        array = np.vstack([array])
+        prediction = model.predict(array)
+        return prediction
 
 if __name__ == "__main__":
     print("===== ## This is Main Block of the Program of 'gesture_verify.py' ## =====")
@@ -361,9 +388,11 @@ if __name__ == "__main__":
             labels = (train_generator.class_indices)
             labels = dict((v,k) for k,v in labels.items())
             print("The Training Labels are ================================\n",labels,'\n=================================================')
-            model=NeuralNetBuilder()
+            #model=NeuralNetBuilder()
+            model=SVMModel()
             ModelSaver(model)
-            ModelTrainer(model,train_generator,validation_generator)
+            x_train,y_train,x_val,y_val=DataSplitter(train_generator,validation_generator)
+            ModelTrainer(model,train_generator,validation_generator,False,x_train,y_train)
         if choose=='n':
             model=ModelLoader()
     from keras.preprocessing import image
@@ -371,7 +400,7 @@ if __name__ == "__main__":
     while True:
         ftypes = [('Jpg File',"*.jpg")]
         tt1  = "Picture Picker"
-        path=NotFoundSoln(ftypes,tt1)
+        path=custom_utilities.FileFolderPicker(ftypes,tt1)
         img = image.load_img(path, target_size=(150, 150))
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
@@ -382,8 +411,17 @@ if __name__ == "__main__":
         if keypress == ord("q"):
             print("Exit Key Pressed")
             break
-        
+
     print("\nPro Tip : You can Make Gesture Predictions using this Module")
     print("\n====== ## Main Block DOne ##======")
+elif __name__ == "gesture_verify":
+    print("Import Found======================")
+    try:
+        from cv2 import resize
+        from tensorflow import cast,float32
+    except Exception as exp:
+        print(" !! Error !! : \n\t",exp)
+else:
+    print("Dunno.But Import Found as : **",__name__)
 
-print("\n\tEND of 'gesture_verify.py'")
+print("\n\tEND of 'gesture_verify.py'" )
