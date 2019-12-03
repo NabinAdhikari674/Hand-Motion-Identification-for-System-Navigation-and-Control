db3c8835aef22b4ecbef095d98310f28060f8b07
Nabin Adhikari <36256500+NabinAdhikari674@users.noreply.github.com>
2019-12-03 18:43:44 +0530

    Feature Update and Bug Fixes

--- sources/gesture_verify.py
+++ sources/gesture_verify.py
@@ -142,7 +142,7 @@ def NeuralNetBuilder():
     model = tf.keras.models.Sequential([
                                         # Note the input shape is the desired size of the image 150x150 with 3 bytes color
                                         # This is the first convolution
-                                        tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3)),
+                                        tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150,1)),
                                         tf.keras.layers.MaxPooling2D(2, 2),
                                         # The second convolution
                                         tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
@@ -153,12 +153,18 @@ def NeuralNetBuilder():
                                         # The fourth convolution
                                         tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
                                         tf.keras.layers.MaxPooling2D(2,2),
+                                        # The Fifth Convolution
+                                        tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+                                        #tf.keras.layers.MaxPooling2D(2,2),
+                                        # The Sixth convolution
+                                        tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+                                        #tf.keras.layers.MaxPooling2D(2,2),
                                         # Flatten the results to feed into a DNN
                                         tf.keras.layers.Flatten(),
                                         tf.keras.layers.Dropout(0.5),
                                         # 512 neuron hidden layer
-                                        tf.keras.layers.Dense(512, activation='relu'),
-                                        tf.keras.layers.Dense(7, activation='softmax')
+                                        tf.keras.layers.Dense(512, activation='tanh'),
+                                        tf.keras.layers.Dense(9, activation='sigmoid')
                                         ])
 
     model.summary()
